92e0f0db63e171d6f141f80b0f22c724fba09542
Martha <marthanyangatiwaithaka@gmail.com>
2024-05-14 08:30:24 +0300

    new commit

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -13,7 +13,7 @@ class BotsPage extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:6001/bots')
+    fetch('https://json-server-3-dp5g.onrender.com/bots')
       .then(response => response.json())
       .then(bots => this.setState({ botCollection: bots, filteredCollection: bots }))
       .then(console.log("Bots Fetched!"))
@@ -47,7 +47,7 @@ class BotsPage extends Component {
 
     this.setState({ botCollection: newCollection, filteredCollection: newFilteredCollection, botArmy: newArmy })
 
-    fetch(`http://localhost:6001/bots/${bot.id}`, {
+    fetch(`https://json-server-3-dp5g.onrender.com/bots${bot.id}`, {
       method: 'DELETE'
     }).then(response => response.json())
       .then(result => console.log(result))
